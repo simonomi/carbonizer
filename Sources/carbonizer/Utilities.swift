5b2131423b6fd912e379c563d36876cd64f821e6
simonomi <thesimonpellerin@gmail.com>
2024-07-27 23:36:03 -0700

    update share permissions when setting creation date on windows

--- Sources/carbonizer/Utilities.swift
+++ Sources/carbonizer/Utilities.swift
@@ -174,24 +174,19 @@ extension URL {
 #if os(Windows)
 		let pathLength = path.count + 1
 		
-		print("path", path)
-		print("count", path.count)
-		print("utf16 count", path.utf16.count)
-		print("utf16 c string count", path.cString(using: .utf16)?.count)
-		print()
-		
 		let windowsFilePath = path.withCString(encodedAs: UTF16.self) {
 			let buffer = UnsafeMutablePointer<UInt16>.allocate(capacity: pathLength)
 			buffer.initialize(from: $0, count: pathLength)
 			return UnsafePointer(buffer)
 		}
-		let hFile = CreateFileW(windowsFilePath, DWORD(GENERIC_WRITE), DWORD(FILE_SHARE_WRITE), nil, DWORD(OPEN_EXISTING), 0, nil)
 		
+		let shareAll = FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE
+		let hFile = CreateFileW(windowsFilePath, GENERIC_WRITE, shareAll, nil, OPEN_EXISTING, 0, nil)
 		if hFile == INVALID_HANDLE_VALUE {
 			throw WindowsError(code: GetLastError())
 		}
-		
 		defer { CloseHandle(hFile) }
+		
 		var creationTime = FILETIME(from: time_t(date.timeIntervalSince1970))
 		guard SetFileTime(hFile, &creationTime, nil, nil) else {
 			throw WindowsError(code: GetLastError())
